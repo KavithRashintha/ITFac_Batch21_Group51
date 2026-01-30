4f9fcf9cf9b5471aff4be8969107dd1b1259ca59
KavithRashintha <wgkrashintha@gmail.com>
2026-01-30 18:20:09 +0530

    fix: replace global variable exports with 'this.page' to resolve scope errors

--- src/test/steps/ui/loginSteps.js
+++ src/test/steps/ui/loginSteps.js
@@ -1,37 +1,30 @@
 import {Given, When, Then} from "@cucumber/cucumber";
 import { chromium } from '@playwright/test';
 
-export let browser;
-let context;
-export let page;
-
 Given('User logged-in as Admin', async function () {
-    console.log("URL Provided for ui")
 
-    browser = await chromium.launch({ headless: false});
-    context = await browser.newContext();
-    page = await context.newPage();
+    this.browser = await chromium.launch({ headless: false});
+    this.context = await this.browser.newContext();
+    this.page = await this.context.newPage();
 
-    await page.goto('http://localhost:8080/ui/login');
+    await this.page.goto('http://localhost:8080/ui/login');
 });
 
 
 When('Provide valid username {string} and password {string} for ui', async function (username, password) {
-    console.log("Username and Password provided for ui")
 
-    await  page.fill('input[name="username"]', username);
-    await page.fill('input[name="password"]', password);
+    await  this.page.fill('input[name="username"]', username);
+    await this.page.fill('input[name="password"]', password);
 });
 
 
 Then('click the login button for ui', async function () {
-    console.log("Login button clicked for ui and navigating to dashboard")
 
-    await page.click('button[type="submit"]');
+    await this.page.click('button[type="submit"]');
 
     // wait until login finishes
-    await page.waitForURL('**/ui/**');
-    await page.goto('http://localhost:8080/ui/dashboard');
+    await this.page.waitForURL('**/ui/**');
+    await this.page.goto('http://localhost:8080/ui/dashboard');
 
     // await browser.close();
 });
