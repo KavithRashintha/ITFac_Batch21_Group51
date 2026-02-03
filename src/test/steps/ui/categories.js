66e4def8905692a613232def247d0a1540691200
Rajinie Samarasinghe <110462570+Samarasinghe-MRR@users.noreply.github.com>
2026-02-03 14:40:10 +0530

    Category UI test cases - TC_ADMIN_CAT_01, TC_ADMIN_CAT_02 completed with cloned code

--- src/test/steps/ui/categories.js
+++ src/test/steps/ui/categories.js
@@ -1,28 +1,29 @@
 import { When, Then } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
 
-// ---------------- Verify the Edit category button in Actions ----------------
+//--------------------------- Verify the Edit category button in Actions ---------------------------
 
 When('user navigates to {string}', async function (path) {
-  await this.page.goto(`http://localhost:8081/${path}`);
+    await this.page.goto(`http://localhost:8081/${path}`);
 });
 
 When('user clicks on Edit button', async function () {
-  await this.page.locator('[title="Edit"]').first().click();
+    await this.page.locator('[title="Edit"]').first().click();
 });
 
-Then('user navigates to edit category page', async function () {
-  await this.page.waitForURL('**/ui/categories/edit/**');
+Then('user navigates to edit category page', async function(){
+    await this.page.waitForURL('**/ui/categories/edit/**');
 });
 
-// -------- Verify validation errors when editing category name --------
 
-When('user provide categoryName {string}', async function (newCategoryName) {
-  await this.page.fill('input[name="name"]', newCategoryName);
-});
+//-------------- Verify the Validation errors when editing a categoryname that not meet the valid criteria (3-10)------------
+
+When('user provide categoryName {string}', async function(newCategoryName){
+    await this.page.fill('input[name="name"]',newCategoryName);
+} );
 
 Then('user click save button', async function () {
-  await this.page.click('button[type="submit"]');
+    await this.page.click('button[type="submit"]');
 });
 
 // ---------- View Categories ----------
