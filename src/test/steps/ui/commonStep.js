dd8fb110a0267febb1d630b95f4865e35a62e5e9
Dinuka-SID <dmcoder01@gmail.com>
2026-02-03 22:46:00 +0530

    small bug fixed

--- src/test/steps/ui/commonStep.js
+++ src/test/steps/ui/commonStep.js
@@ -1,30 +1,30 @@
-import { When, Then, After } from "@cucumber/cucumber";
-import { expect } from "@playwright/test";
+// import { When, Then, After } from "@cucumber/cucumber";
+// import { expect } from "@playwright/test";
 
-const BASE_URL = "http://localhost:8080";
+// const BASE_URL = "http://localhost:8080";
 
-/**
- * Navigation step (REUSABLE)
- */
-When("User navigates to {string}", async function (path) {
-  await this.page.goto(`${BASE_URL}${path}`);
-});
+// /**
+//  * Navigation step (REUSABLE)
+//  */
+// When("User navigates to {string}", async function (path) {
+//   await this.page.goto(`${BASE_URL}${path}`);
+// });
 
-/**
- * Generic button visibility step (REUSABLE)
- */
-Then("The {string} button should be visible", async function (buttonName) {
-  const button =
-    (await this.page.getByRole("button", { name: buttonName }).count()) > 0
-      ? this.page.getByRole("button", { name: buttonName })
-      : this.page.getByRole("link", { name: buttonName });
+// /**
+//  * Generic button visibility step (REUSABLE)
+//  */
+// Then("The {string} button should be visible", async function (buttonName) {
+//   const button =
+//     (await this.page.getByRole("button", { name: buttonName }).count()) > 0
+//       ? this.page.getByRole("button", { name: buttonName })
+//       : this.page.getByRole("link", { name: buttonName });
 
-  await expect(button).toBeVisible();
-});
+//   await expect(button).toBeVisible();
+// });
 
-/**
- * Cleanup and  close browser after each scenario (REUSABLE)
- */
-After(async function () {
-  await this.browser.close();
-});
+// /**
+//  * Cleanup and  close browser after each scenario (REUSABLE)
+//  */
+// After(async function () {
+//   await this.browser.close();
+// });
